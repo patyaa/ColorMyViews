7371246c2e35b1a8e9438f9b823f13a2ff58d492
patyaa <b.patrik972531@gmail.com>
2021-06-24 11:10:09 +0200

    dataBinding

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -1,36 +1,34 @@
 package com.example.colormyviews
 
-
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.colormyviews.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setListeners()
     }
 
     private fun setListeners() {
-
         val clickableViews: List<View> =
             listOf(
-                R.id.box_one_text,
-                R.id.box_one_text,
-                R.id.box_two_text,
-                R.id.box_three_text,
-                R.id.box_four_text,
-                R.id.box_five_text,
-                R.id.constraint_layout,
-                R.id.red_button,
-                R.id.yellow_button,
-                R.id.green_button
+                binding.boxOneText,
+                binding.boxTwoText,
+                binding.boxThreeText,
+                binding.boxFourText,
+                binding.boxFiveText,
+                binding.constraintLayout,
+                binding.redButton,
+                binding.yellowButton,
+                binding.greenButton
             )
-                .map { findViewById(it) }
-
         for (item in clickableViews) {
             item.setOnClickListener { makeColored(it) }
         }
@@ -46,9 +44,11 @@ class MainActivity : AppCompatActivity() {
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
 
-            R.id.red_button -> findViewById<View>(R.id.box_three_text).setBackgroundResource(R.color.my_red)
-            R.id.yellow_button -> findViewById<View>(R.id.box_four_text).setBackgroundResource(R.color.my_yellow)
-            R.id.green_button -> findViewById<View>(R.id.box_five_text).setBackgroundResource(R.color.my_green)
+            R.id.red_button -> binding.boxThreeText.setBackgroundResource(android.R.color.holo_blue_dark)
+            R.id.yellow_button -> binding.boxFourText.setBackgroundResource(android.R.color.black)
+            R.id.green_button -> binding.boxFiveText.setBackgroundResource(android.R.color.darker_gray)
+
+
             //TODO databinding
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
